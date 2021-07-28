ba2709001120f33bb0ea5fd1691b6240cf1ed27d
IdeaLim <wiuand@naver.com>
2021-07-28 09:29:54 +0900

    First Commit

---
+++ app/src/main/java/com/example/composecookbookbasic/ui/data/DemoDataProvider.kt
@@ -0,0 +1,29 @@
+package com.example.composecookbookbasic.ui.data
+
+import com.example.composecookbookbasic.R
+
+object DemoDataProvider {
+    val itemList = listOf(
+        Item(
+            1,
+            "Taipei",
+            imageId = R.drawable.taipei1,
+            0,
+            0
+        ),
+        Item(
+            2,
+            "Taipei",
+            imageId = R.drawable.taipei2,
+            0,
+            0
+        ),
+        Item(
+            3,
+            "Taipei",
+            imageId = R.drawable.taipei3,
+            0,
+            0
+        )
+    )
+}
